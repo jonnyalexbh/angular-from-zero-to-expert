a6018918be82d8bb1a95a009fe0c689893c7e294
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-06 11:34:40 -0500

    creating the footer.component

---
+++ hello-world/src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+  year: number;
+
+  /**
+  * constructor
+  *
+  */
+  constructor() {
+    this.year = new Date().getFullYear();
+  }
+}
