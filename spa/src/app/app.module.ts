a58bf29b5e0e0955afb2f43c61784309108b3db1
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-07 16:03:51 -0500

    routes in Angular

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routes
+import { APP_ROUTING } from './app.routes';
+
+// services
+
+// components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +22,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
