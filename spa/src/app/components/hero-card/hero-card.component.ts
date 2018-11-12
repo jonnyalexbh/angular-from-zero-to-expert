7b6bb1d890849eb3d591f1a29f901d2b01bd4206
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-12 12:56:01 -0500

    @Output - issue an event of the son towards the father

--- spa/src/app/components/hero-card/hero-card.component.ts
+++ spa/src/app/components/hero-card/hero-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,11 @@ export class HeroCardComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() idx: number;
 
-  constructor(private _router: Router) { }
+  @Output() selectedHero: EventEmitter<number>;
+
+  constructor(private _router: Router) {
+    this.selectedHero = new EventEmitter;
+  }
 
   ngOnInit() {
   }
@@ -21,7 +25,8 @@ export class HeroCardComponent implements OnInit {
    *
    */
   seeHero() {
-    this._router.navigate(['/heroe', this.idx]);
+    // this._router.navigate(['/heroe', this.idx]);
+    this.selectedHero.emit(this.idx);
   }
 
 }
