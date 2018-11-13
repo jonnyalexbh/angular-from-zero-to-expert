fc1b045b682a698fd11696792a54425cc35dde86
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-13 13:48:41 -0500

    date Pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -30,4 +30,6 @@ export class AppComponent {
 
   });
 
+  dateRecord = new Date();
+
 }
--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
