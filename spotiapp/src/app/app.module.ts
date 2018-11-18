0ca9d07a9ccf9f5552eb1255a3c58eaf3f3d3ffa
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-18 00:30:59 -0500

    introduction to HTTP requests

--- spotiapp/src/app/app.module.ts
+++ spotiapp/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// http request
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
@@ -21,6 +24,7 @@ import { ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [],
--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[] = [];
+
+  constructor(private http: HttpClient) {
+    console.log('builder called');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe((resp: any) => {
+        this.countries = resp;
+      });
+  }
 
   ngOnInit() {
   }
