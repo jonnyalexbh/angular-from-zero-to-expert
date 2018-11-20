fe82ab10df35cda4aaee5e4c2510ffd9cb58c603
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-19 22:57:42 -0500

    artist search component

--- spotiapp/src/app/components/search/search.component.ts
+++ spotiapp/src/app/components/search/search.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artists: any[] = [];
 
-  ngOnInit() {
+  /**
+   * constructor
+   *
+   */
+  constructor(private spotify: SpotifyService) { }
+
+  /**
+   * search
+   *
+   */
+  search(term: string) {
+    this.spotify.getArtist(term).subscribe((data: any) => {
+      this.artists = data.artists.items;
+    });
   }
 
 }
--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -19,9 +19,22 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCP7FrHfjvjaNaWzO1kDizLjNCjklPj3bj0468rpHT3ikiMeT_tAnMQ0obgkea_4OzivU-imR3gw8pV3h8'
+      'Authorization': 'Bearer BQDO12P54HoUvswdjRXMbd_x-BOUovXqQzxtjjA7xFFh6VPzUWNDAUYdRfIJcUTfbIO7QA2rf4y8m270BrQ'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+  }
+
+  /**
+   * getArtist
+   *
+   */
+  getArtist(term: string) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDO12P54HoUvswdjRXMbd_x-BOUovXqQzxtjjA7xFFh6VPzUWNDAUYdRfIJcUTfbIO7QA2rf4y8m270BrQ'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers });
   }
 }
