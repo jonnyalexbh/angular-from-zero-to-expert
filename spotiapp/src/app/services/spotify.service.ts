74b50f313dee271ba66a31eeeb113e71bb8704eb
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-21 08:47:50 -0500

    centralize requests to Spotify

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -14,21 +14,29 @@ export class SpotifyService {
   constructor(private http: HttpClient) { }
 
   /**
+   * getQuery
+   *
+   */
+  getQuery(query: string) {
+
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDS2vygg75x9REAdm28qA90vFeTQpyjpX3Kc48EbHS67UiU93HaaeiUbSVEqC8fg87LdLOTZaz-DS5M5xg'
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+  /**
    * getNewReleases
    *
    */
   getNewReleases() {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDWteHxGC5rFUe91N39hdlUOf1E9F0kzcB9LeyGzZgSGCMCG8U0plgDwcGBTkFgQc1Gmpnu97vHLyKV678'
-    });
-
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+    return this.getQuery('browse/new-releases?limit=20')
       .pipe(map(data => data['albums'].items));
-
-    // .pipe(map(data => {
-    //   return data['albums'].items;
-    // }));
   }
 
   /**
@@ -37,11 +45,7 @@ export class SpotifyService {
    */
   getArtist(term: string) {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDWteHxGC5rFUe91N39hdlUOf1E9F0kzcB9LeyGzZgSGCMCG8U0plgDwcGBTkFgQc1Gmpnu97vHLyKV678'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers })
+    return this.getQuery(`search?q=${term}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items));
   }
 }
