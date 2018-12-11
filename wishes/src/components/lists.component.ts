bab4777b1954d5c89f56c6e4bb8690dd5039012c
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-12-10 23:13:48 -0500

    edit the title of the list

--- wishes/src/components/lists.component.ts
+++ wishes/src/components/lists.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { WishesService } from "../services/wishes.service";
-import { NavController } from "ionic-angular";
+import { NavController, AlertController, ItemSliding } from "ionic-angular";
 import { AddPage } from "../pages/add/add.component";
 import { List } from "../models";
 
@@ -13,7 +13,8 @@ export class ListsComponent {
   @Input() completed: boolean = false;
 
   constructor(public wishesService: WishesService,
-    private navCtrl: NavController) {
+    private navCtrl: NavController,
+    private alertCtrl: AlertController) {
 
   }
 
@@ -26,6 +27,43 @@ export class ListsComponent {
       title: list.title,
       list: list
     });
+  }
+
+  /**
+   * editList
+   *
+   */
+  editList(list: List, slidingItem: ItemSliding) {
+
+    slidingItem.close();
+
+    const alert = this.alertCtrl.create({
+      title: 'Edit name',
+      message: "edit the name of the list",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'name of the list',
+          value: list.title,
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        }, {
+          text: 'Save',
+          handler: data => {
+            if (data.title.length === 0) {
+              return;
+            }
+            list.title = data.title;
+            this.wishesService.saveStorage();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
   /**
